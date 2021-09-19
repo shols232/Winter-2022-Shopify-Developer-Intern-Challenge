ead8959e5d97089de52d3de76eeedb6f7434718b
ademola <ademola@quickcheck.ng>
2021-09-19 16:11:26 +0100

    Improve docs

--- images/serializers.py
+++ images/serializers.py
@@ -7,14 +7,20 @@ from rest_framework.fields import CharField
 
 
 class ImageSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(required=True, validators=[validate_image_file_size, validate_image_extension])
+    image = serializers.ImageField(
+        required=True, validators=[validate_image_file_size, validate_image_extension]
+    )
     name = serializers.CharField(source="image.name", read_only=True)
     owner = serializers.CharField(source="owner.username", read_only=True)
 
     class Meta:
         model = UserImage
-        fields = ["owner", "image", "name", "times_shared", "size"]
-        extra_kwargs = {'size': {'read_only': True}, 'times_shared': {'read_only': True}}
+        fields = ["owner", "image", "name", "times_shared", "size", "private"]
+        extra_kwargs = {
+            "size": {"read_only": True},
+            "times_shared": {"read_only": True},
+            "private": {"write_only": True}
+        }
 
 
 class ShareImageSerializer(serializers.Serializer[Dict[str, str]]):
